878f4782b5ee8ea1353e0521a0245192e9fea78f
keisuke-kk <candy0220.k@gmail.com>
2020-05-16 22:03:13 +0900

    k

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
   }
   root 'items#index'
-<<<<<<< HEAD
-  resources :items, only: [:index, :show]
-=======
   resources :users, only: [:index, :show, :edit, :update, :new]
   resources :items, only: [:index, :show, :new, :create] do
     resources :comment, only: :create
@@ -13,6 +10,5 @@ Rails.application.routes.draw do
     resources :brand, only: :new
     resources :shipping, only: :new
   end
->>>>>>> ef4b7de580a9aad2565c4402233b46d01b074f96
   resources :item_payment
 end
