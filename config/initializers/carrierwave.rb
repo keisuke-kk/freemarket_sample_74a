f7b8803f7a2343317d98f66a5c45d43c2fd0a9ec
keisuke-kk <candy0220.k@gmail.com>
2020-10-07 15:47:11 +0900

    commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,11 +10,17 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1'
+      region: ENV['AWS_S3_REGION'],
+      aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_IAM_ACCESS_KEY'],
+      # aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      # aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      # region: 'ap-northeast-1'
     }
-    config.fog_directory  = 'f74a'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/f74a'
+    config.fog_directory = ENV['AWS_S3_BUCKET']
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+    CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+    # config.fog_directory  = 'f74a'
+    # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/f74a'
   end
 end
